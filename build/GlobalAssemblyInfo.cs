854620f5b9de7080bf9e42122560f44afbbf2e78
bubibubi <u.ballestrazzi@gmail.com>
2017-11-18 11:12:09 +0100

    Add missing build files

---
+++ build/GlobalAssemblyInfo.cs
@@ -0,0 +1,22 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
+
+[assembly: AssemblyCompany("Bubi")]
+[assembly: AssemblyProduct("EntityFrameworkCore.Jet")]
+[assembly: AssemblyCopyright("Copyright © 2017 - Bubi")]
+[assembly: AssemblyTrademark("Before use any of the trademarks, check if they are registered")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e77112b7-b6ba-43a8-b21f-dd79a7a47a2f")]
+
+[assembly: AssemblyVersion("2.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.7")]
+[assembly: AssemblyInformationalVersion("2.0.0")]

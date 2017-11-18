fb281dca1fc9b68bb9b3950813ae902efd440bf4
bubibubi <u.ballestrazzi@gmail.com>
2017-11-18 11:12:54 +0100

    msbuild updates

--- msbuild/_MakeSetup_Pack.bat
+++ msbuild/_MakeSetup_Pack.bat
@@ -1,3 +1,3 @@
-@echo Deleting al package files >> Output\Makesetup.log
+@echo Deleting all package files >> Output\Makesetup.log
 del /q Output\*.nupkg >> Output\Makesetup.log
 nuget pack EFCore.Jet.nuspec -version %VERSION% -OutputDirectory Output >> Output\Makesetup.log
--- msbuild/_MakeSetup_Push.bat
+++ msbuild/_MakeSetup_Push.bat
@@ -1,4 +1,4 @@
 @echo Press enter to upload nuget files
 @pause
-SET PARAMETERS= -Verbosity detail
+SET PARAMETERS= -Verbosity detail -Source https://www.nuget.org/api/v2/package/
 FOR %%F IN (output\*.nupkg) DO nuget push %%F %PARAMETERS% >>  output\Makesetup.log

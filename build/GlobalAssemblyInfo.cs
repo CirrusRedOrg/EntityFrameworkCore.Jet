9733c5e5fd9268b2bb7ee5d6f5627ba0e035e1b8
bubibubi <u.ballestrazzi@gmail.com>
2018-06-07 16:21:01 +0200

    Release 2.1.0-preview1

--- build/GlobalAssemblyInfo.cs
+++ build/GlobalAssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyCompany("Bubi")]
 [assembly: AssemblyProduct("EntityFrameworkCore.Jet")]
-[assembly: AssemblyCopyright("Copyright © 2017 - Bubi")]
+[assembly: AssemblyCopyright("Copyright © 2017-2018 - Bubi")]
 [assembly: AssemblyTrademark("Before use any of the trademarks, check if they are registered")]
 [assembly: AssemblyCulture("")]
 
@@ -17,6 +17,6 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("e77112b7-b6ba-43a8-b21f-dd79a7a47a2f")]
 
-[assembly: AssemblyVersion("2.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.7")]
-[assembly: AssemblyInformationalVersion("2.0.0")]
+[assembly: AssemblyVersion("2.1.0")]
+[assembly: AssemblyFileVersion("2.1.0.7")]
+[assembly: AssemblyInformationalVersion("2.1.0-preview1")]

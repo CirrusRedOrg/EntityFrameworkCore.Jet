848defc96f9981524e68b770f4d00b81e322127c
bubibubi <u.ballestrazzi@gmail.com>
2018-06-07 16:43:50 +0200

    Release 2.1.0-preview3 Fixed NuGet dependencies

--- build/GlobalAssemblyInfo.cs
+++ build/GlobalAssemblyInfo.cs
@@ -19,4 +19,4 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("2.1.0")]
 [assembly: AssemblyFileVersion("2.1.0.0")]
-[assembly: AssemblyInformationalVersion("2.1.0-preview2")]
+[assembly: AssemblyInformationalVersion("2.1.0-preview3")]

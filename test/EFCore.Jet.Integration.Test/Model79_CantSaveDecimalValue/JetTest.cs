42c4aabdfd56e8f081b2c4c09059213cced25fb0
bubibubi <u.ballestrazzi@gmail.com>
2019-08-02 11:34:23 +0200

    Added test case for issue 32

---
+++ test/EFCore.Jet.Integration.Test/Model79_CantSaveDecimalValue/JetTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Data.Common;
+using System.Data.Jet;
+using System.Data.OleDb;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EFCore.Jet.Integration.Test.Model79_CantSaveDecimalValue
+{
+    [TestClass]
+    public class Model79_CantSaveDecimalValue : Test
+    {
+        protected override DbConnection GetConnection()
+        {
+            // ReSharper disable once CollectionNeverUpdated.Local
+
+            OleDbConnectionStringBuilder oleDbConnectionStringBuilder = new OleDbConnectionStringBuilder();
+            //oleDbConnectionStringBuilder.Provider = "Microsoft.Jet.OLEDB.4.0";
+            //oleDbConnectionStringBuilder.DataSource = @".\Empty.mdb";
+            oleDbConnectionStringBuilder.Provider = "Microsoft.ACE.OLEDB.15.0";
+            oleDbConnectionStringBuilder.DataSource = Helpers.GetTestDirectory() + "\\BrandNewDatabase.accdb";
+            return new JetConnection(oleDbConnectionStringBuilder.ToString());
+        }
+    }
+}
---
+++ test/EFCore.Jet.Integration.Test/Model79_CantSaveDecimalValue/Table.cs
@@ -0,0 +1,8 @@
+﻿namespace EFCore.Jet.Integration.Test.Model79_CantSaveDecimalValue
+{
+    public class Table
+    {
+        public int Id { get; set; }
+        public decimal DecimalValue { get; set; }
+    }
+}

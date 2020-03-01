6fffce90a8b7cfdaaf7e0499c8934323b300f237
Lau <laucomm@gmail.com>
2020-03-01 15:15:43 +0100

    Upgrade scaffolding related files to 3.1.x. The Jet DatabaseModelFactory class needs to be checked and tested and its code similarity to the SQL Server provider should be increased.

--- src/EFCore.Jet/Scaffolding/Internal/JetCodeGenerator.cs
+++ src/EFCore.Jet/Scaffolding/Internal/JetCodeGenerator.cs
@@ -1,11 +1,19 @@
-﻿// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using JetBrains.Annotations;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
 using Microsoft.EntityFrameworkCore.Scaffolding;
 
 namespace EntityFrameworkCore.Jet.Scaffolding.Internal
 {
+    /// <summary>
+    ///     This is an internal API that supports the Entity Framework Core infrastructure and not subject to
+    ///     the same compatibility standards as public APIs. It may be changed or removed without notice in
+    ///     any release. You should only use it directly in your code with extreme caution and knowing that
+    ///     doing so can result in application failures when updating to a new Entity Framework Core release.
+    /// </summary>
     public class JetCodeGenerator : ProviderCodeGenerator
     {
         /// <summary>
@@ -18,17 +26,18 @@ namespace EntityFrameworkCore.Jet.Scaffolding.Internal
         }
 
         /// <summary>
-        ///     This API supports the Entity Framework Core infrastructure and is not intended to be used
-        ///     directly from your code. This API may change or be removed in future releases.
+        ///     This is an internal API that supports the Entity Framework Core infrastructure and not subject to
+        ///     the same compatibility standards as public APIs. It may be changed or removed without notice in
+        ///     any release. You should only use it directly in your code with extreme caution and knowing that
+        ///     doing so can result in application failures when updating to a new Entity Framework Core release.
         /// </summary>
         public override MethodCallCodeFragment GenerateUseProvider(
             string connectionString,
             MethodCallCodeFragment providerOptions)
             => new MethodCallCodeFragment(
-                nameof(JetDbContextOptionsExtensions.UseJet),
+                nameof(JetDbContextOptionsBuilderExtensions.UseJet),
                 providerOptions == null
                     ? new object[] { connectionString }
                     : new object[] { connectionString, new NestedClosureCodeFragment("x", providerOptions) });
-
     }
 }

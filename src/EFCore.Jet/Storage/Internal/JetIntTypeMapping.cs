cda2dce9cedd1cff75b53de1bfcdd6b9ce04b2f4
Laurents Meyer <laucomm@gmail.com>
2021-12-14 15:14:14 +0100

    Improve counter type handling. (#112)

---
+++ src/EFCore.Jet/Storage/Internal/JetIntTypeMapping.cs
@@ -0,0 +1,24 @@
+﻿using JetBrains.Annotations;
+using Microsoft.EntityFrameworkCore.Storage;
+
+namespace EntityFrameworkCore.Jet.Storage.Internal
+{
+    public class JetIntTypeMapping : IntTypeMapping
+    {
+        public JetIntTypeMapping([NotNull] string storeType)
+            : base(storeType, System.Data.DbType.Int32)
+        {
+        }
+        
+        protected JetIntTypeMapping(RelationalTypeMappingParameters parameters)
+            : base(parameters)
+        {
+        }
+
+        // JetIntTypeMapping is also used for an explicit counter type, because we actually want it to be integer unless
+        // the value generation type is also OnAdd.
+        // We therefore lock the store type to its original value (which should be "integer").
+        protected override RelationalTypeMapping Clone(RelationalTypeMappingParameters parameters)
+            => new JetIntTypeMapping(parameters.WithStoreTypeAndSize(Parameters.StoreType, parameters.Size));
+    }
+}

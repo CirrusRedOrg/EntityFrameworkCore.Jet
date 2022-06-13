6a8ccd27de7e9609c1003ff1ab3faffc6470bc40
Laurents Meyer <laucomm@gmail.com>
2022-06-13 22:04:50 +0200

    Fix transactions issues (#129)
    * Fix active transaction support and disposed handling.
    
    * Add transaction baseline tests.
    
    * Fix transaction tests.

--- src/EFCore.Jet.Data/JetTransaction.cs
+++ src/EFCore.Jet.Data/JetTransaction.cs
@@ -1,11 +1,15 @@
-﻿using System.Data;
+﻿using System;
+using System.Data;
 using System.Data.Common;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace EntityFrameworkCore.Jet.Data
 {
     internal class JetTransaction : DbTransaction
     {
-        private readonly JetConnection _connection;
+        private JetConnection _connection;
+        private bool _disposed;
 
         internal virtual DbTransaction WrappedTransaction { get; }
 
@@ -22,8 +26,12 @@ namespace EntityFrameworkCore.Jet.Data
 
         public override void Commit()
         {
+            if (_disposed)
+                throw new ObjectDisposedException(nameof(JetTransaction));
+            
             LogHelper.ShowCommandHeader("--- Commit");
             WrappedTransaction.Commit();
+
             _connection.ActiveTransaction = null;
         }
 
@@ -35,9 +43,63 @@ namespace EntityFrameworkCore.Jet.Data
 
         public override void Rollback()
         {
+            if (_disposed)
+                throw new ObjectDisposedException(nameof(JetTransaction));
+            
             LogHelper.ShowCommandHeader("^^^ Rollback");
             WrappedTransaction.Rollback();
+
             _connection.ActiveTransaction = null;
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            try
+            {
+                if (disposing)
+                {
+                    if (_connection?.ActiveTransaction == this)
+                    {
+                        if (Connection.State == ConnectionState.Open)
+                        {
+                            Rollback();
+                        }
+
+                        _connection.ActiveTransaction = null;
+                    }
+                }
+            }
+            finally
+            {
+                _disposed = true;
+                _connection = null;
+
+                base.Dispose(disposing);
+            }
+        }
+
+        public override Task CommitAsync(CancellationToken cancellationToken = new CancellationToken())
+        {
+            if (_disposed)
+                throw new ObjectDisposedException(nameof(JetTransaction));
+            
+            return base.CommitAsync(cancellationToken);
+        }
+
+        public override ValueTask DisposeAsync()
+        {
+            if (_disposed)
+                throw new ObjectDisposedException(nameof(JetTransaction));
+            
+            return base.DisposeAsync();
+        }
+
+        public override Task RollbackAsync(CancellationToken cancellationToken = new CancellationToken())
+        {
+            if (_disposed)
+                throw new ObjectDisposedException(nameof(JetTransaction));
+            
+            return base.RollbackAsync(cancellationToken);
         }
     }
 }

acc3fd522349a5fe8744d31020cb1363fbcfc0bd
Christopher Jolly <chris-jolly_au@hotmail.com>
2023-04-26 23:46:59 +0800

    Revert "Set the precision to be maximum of 28 (Jet limit)."
    This reverts commit a1904bf1131308c5c533030988ded74c7e137197.

--- src/EFCore.Jet/Storage/Internal/JetDecimalTypeMapping.cs
+++ src/EFCore.Jet/Storage/Internal/JetDecimalTypeMapping.cs
@@ -53,20 +53,7 @@ namespace EntityFrameworkCore.Jet.Storage.Internal
         ///     doing so can result in application failures when updating to a new Entity Framework Core release.
         /// </summary>
         protected override RelationalTypeMapping Clone(RelationalTypeMappingParameters parameters)
-        {
-            var precision = parameters.Precision;
-            var scale = parameters.Scale;
-            if (parameters.Precision is > 28)
-            {
-                int prec_diff = parameters.Precision.Value - 28;
-                precision = 28;
-                if (parameters.Scale is > 28)
-                {
-                    scale = parameters.Scale.Value - prec_diff;
-                }
-            }
-            return new JetDecimalTypeMapping(parameters.WithPrecisionAndScale(precision, scale));
-        }
+            => new JetDecimalTypeMapping(parameters);
 
         /// <summary>
         ///     This is an internal API that supports the Entity Framework Core infrastructure and not subject to

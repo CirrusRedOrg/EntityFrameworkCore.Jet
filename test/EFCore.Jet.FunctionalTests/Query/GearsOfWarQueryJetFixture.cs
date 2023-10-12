7bc7ee370836f2bdbb1275c815f2d97daa5dcd61
Laurents Meyer <laucomm@gmail.com>
2023-10-12 13:10:54 +0200

    Revert pull-in of GearsOfWar related classes and remove constraint (#145)
    * Revert pull-in of GearsOfWar related classes.
    
    * Clean-up GearsOfWar fixtures.
    
    * Drop constraint to workaround Jet limitation regarding compound foreign keys and NULL.
    
    * Fix SQL assertions.
    
    * Revert "Add code to add a "MatchSimple" annotation to a foreign key", because it doesn't do anything at the moment.
    
    This reverts commit 76408338e02003fa536543d60af9b909a55dfeae.

--- test/EFCore.Jet.FunctionalTests/Query/GearsOfWarQueryJetFixture.cs
+++ test/EFCore.Jet.FunctionalTests/Query/GearsOfWarQueryJetFixture.cs
@@ -1,11 +1,10 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
-using System;
-using EFCore.Jet.CustomBaseTests.GearsOfWarModel;
 using EntityFrameworkCore.Jet.FunctionalTests.TestUtilities;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query;
+using Microsoft.EntityFrameworkCore.TestModels.GearsOfWarModel;
 using Microsoft.EntityFrameworkCore.TestUtilities;
-using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace EntityFrameworkCore.Jet.FunctionalTests.Query
 {
@@ -17,92 +16,15 @@ namespace EntityFrameworkCore.Jet.FunctionalTests.Query
         {
             base.OnModelCreating(modelBuilder, context);
 
-            modelBuilder.Entity<Officer>().HasMany(o => o.Reports).WithOne().HasForeignKey(
-                o => new { o.LeaderNickname, o.LeaderSquadId }).MatchSimple();
-
             modelBuilder.Entity<City>().Property(g => g.Location).HasColumnType("varchar(100)");
-
-            modelBuilder.Entity<Mission>(
-                b =>
-                {
-                    //b.Ignore(m => m.Timeline);
-                    // b.Ignore(m => m.Duration);
-                });
-        }
-
-
-        /*
-         * We need to workaround a different behaviour with Jet.
-         * When working with a foreign key declaration over multiple fields, each field must meet the condition.
-         * This is different to other databases where if any field is null, the foreign key constaint is not required
-         * PostgreSql call this MATCH SIMPLE and MATCH FULL is where each field individually must meet the constraint
-         * SQL Server operates in MATCH SIMPLE mode
-         * Jet differs and operates in MATCH FULL mode
-         */
-        public override ISetSource GetExpectedData()
-        {
-            var data = (GearsOfWarData)base.GetExpectedData();
-
-            /*foreach (var mission in data.Missions)
-            {
-                mission.Timeline = mission.Timeline.AddYears(100);
-            }
-            */
-            /*foreach (var gear in data.Gears)
-            {
-                if (gear.LeaderSquadId != 0) continue;
-                gear.LeaderSquadId = 1;
-                gear.LeaderNickname = "Marcus";
-                ((Officer)gear).Reports.Add(gear);
-            }*/
-            return data;
         }
 
         protected override void Seed(GearsOfWarContext context)
         {
-            var squads = GearsOfWarData.CreateSquads();
-            var missions = GearsOfWarData.CreateMissions();
-            var squadMissions = GearsOfWarData.CreateSquadMissions();
-            var cities = GearsOfWarData.CreateCities();
-            var weapons = GearsOfWarData.CreateWeapons();
-            var tags = GearsOfWarData.CreateTags();
-            var gears = GearsOfWarData.CreateGears();
-            var locustLeaders = GearsOfWarData.CreateLocustLeaders();
-            var factions = GearsOfWarData.CreateFactions();
-            var locustHighCommands = GearsOfWarData.CreateHighCommands();
-            /*foreach (var mission in missions)
-            {
-                mission.Timeline = new DateTimeOffset(new DateTime(1753, 1, 1));
-            }*/
-            /*foreach (var gear in gears)
-            {
-                if (gear.LeaderSquadId != 0) continue;
-                gear.LeaderSquadId = 1;
-                gear.LeaderNickname = "Marcus";
-            }*/
-            GearsOfWarData.WireUp(
-                squads, missions, squadMissions, cities, weapons, tags, gears, locustLeaders, factions, locustHighCommands);
+            // Drop constraint to workaround Jet limitation regarding compound foreign keys and NULL.
+            context.Database.ExecuteSql($"ALTER TABLE `Gears` DROP CONSTRAINT `FK_Gears_Gears_LeaderNickname_LeaderSquadId`");
 
-            /*foreach (var tag in tags)
-            {
-                tag.IssueDate = new DateTime(1750, 1, 1);
-            }*/
-
-
-            context.Squads.AddRange(squads);
-            context.Missions.AddRange(missions);
-            context.Cities.AddRange(cities);
-            context.Weapons.AddRange(weapons);
-            context.Tags.AddRange(tags);
-            context.Gears.AddRange(gears);
-            context.LocustLeaders.AddRange(locustLeaders);
-            context.Factions.AddRange(factions);
-            context.LocustHighCommands.AddRange(locustHighCommands);
-            context.SaveChanges();
-
-            GearsOfWarData.WireUp2(locustLeaders, factions);
-
-            context.SaveChanges();
+            base.Seed(context);
         }
     }
 }

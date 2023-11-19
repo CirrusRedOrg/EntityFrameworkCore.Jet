f121ac68696dde6584315297bdb2034bc62e88ca
Christopher Jolly <chris-jolly_au@hotmail.com>
2023-11-19 20:40:09 +0800

    Remove Complex Navigations Shared classes from the tests. The table structure is too complex for Jet and goes over its limits. Not able to make work

--- test/EFCore.Jet.FunctionalTests/JetComplianceTest.cs
+++ test/EFCore.Jet.FunctionalTests/JetComplianceTest.cs
@@ -12,14 +12,26 @@ namespace EntityFrameworkCore.Jet.FunctionalTests
 
         protected override ICollection<Type> IgnoredTestBases { get; } = new HashSet<Type>
         {
+            //No spatial type support in Jet
             typeof(SpatialQueryRelationalTestBase<>),
             typeof(SpatialQueryTestBase<>),
             typeof(SpatialTestBase<>),
+            //Only very limited support for primitive collections.
+            //Can read/write the whole field at a time but any query that needs access to a specific element will fail.
             typeof(NonSharedPrimitiveCollectionsQueryTestBase),
             typeof(NonSharedPrimitiveCollectionsQueryRelationalTestBase),
+            //No Json query support in Jet
             typeof(JsonQueryTestBase<>),
             typeof(JsonQueryAdHocTestBase),
-            typeof(JsonUpdateTestBase<>)
+            typeof(JsonUpdateTestBase<>),
+            //Too complex table structure for Jet/MS Access. Too many indexes on table.
+            //Caused by having too many navs (foreign keys) on a single table.
+            //Also having a primary key (and its related foreign keys) being over more than 14 fields.
+            typeof(ComplexNavigationsSharedTypeQueryRelationalTestBase<>),
+            typeof(ComplexNavigationsSharedTypeQueryTestBase<>),
+            typeof(ComplexNavigationsCollectionsSharedTypeQueryRelationalTestBase<>),
+            typeof(ComplexNavigationsCollectionsSharedTypeQueryTestBase<>),
+            typeof(ComplexNavigationsCollectionsSplitSharedTypeQueryRelationalTestBase<>)
         };
 
         protected override Assembly TargetAssembly { get; } = typeof(JetComplianceTest).Assembly;

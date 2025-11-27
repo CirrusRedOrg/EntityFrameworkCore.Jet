009fe0631c325d18dd91bbe81a5e3d4d6082ea2c
Christopher Jolly <chris-jolly_au@hotmail.com>
2025-11-28 01:05:43 +0800

    another fix attempt

--- test/EFCore.Jet.Data.Tests/Properties/AssemblyInfo.cs
+++ test/EFCore.Jet.Data.Tests/Properties/AssemblyInfo.cs
@@ -1,3 +1,3 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-[assembly: Parallelize(Scope = ExecutionScope.MethodLevel)]
+[assembly: Parallelize(Workers = 1, Scope = ExecutionScope.MethodLevel)]
